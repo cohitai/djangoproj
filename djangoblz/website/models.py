4b03fc7798238ef7578728c0861d7e2330f7e033
cohitai <cohitai@gmail.com>
2021-02-04 19:13:33 +0100

    second video

---
+++ djangoblz/website/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class articles(models.Model):
+    id = models.PositiveIntegerField(default=0,primary_key=True)
+    url = models.CharField(max_length=200)
+    title = models.TextField(blank=True)
+    lead = models.TextField(blank=True)
+    author = models.CharField(max_length=100)
+    publishdate = models.CharField(max_length=100)
+    text = models.TextField(blank=True)
+    language = models.CharField(max_length=100)
+    image_url = models.TextField(blank=True)
+
+    def __str__(self):
+        return(str(self.id) + " " + self.title)
+
+
