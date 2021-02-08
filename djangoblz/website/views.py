38ae6972f8f7d39caa1c81a7ff5e6e908cff3131
cohitai <cohitai@gmail.com>
2021-02-08 15:52:16 +0100

    database manipulations

--- djangoblz/website/views.py
+++ djangoblz/website/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 
 def home(req):
-    return render(req, 'home.html',{})
+    all_articles = Article.objects.all().order_by('-id')[0:1000]
+    return render(req, 'home.html',{'all':all_articles})
