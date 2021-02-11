71c2dcbb747359230259e67b4d1c16280f1e9b1b
cohitai <cohitai@gmail.com>
2021-02-11 13:16:10 +0100

    migration to a new database

--- djangoblz/website/views.py
+++ djangoblz/website/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Article
+from .models import BerlinerZeitung
 
 # Create your views here.
 
 
 def home(req):
-    all_articles = Article.objects.all().order_by('-id')[0:1000]
-    return render(req, 'home.html',{'all':all_articles})
+    #all_articles = Article.objects.all().order_by('-id')[0:1000]
+    all_articles_blz = BerlinerZeitung.objects.all().order_by('-id')[0:10]
+    return render(req, 'home.html',{'all':all_articles_blz})
